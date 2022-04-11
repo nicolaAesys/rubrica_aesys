45e01840d520a0ea2ff00e32298c82125d4798e0
Davide <davide.demilio@aesys.tech>
2022-04-11 11:36:13 +0200

    create updatecontact

--- src/main/java/com/aesys/rubrica/dao/ConnessioneDao.java
+++ src/main/java/com/aesys/rubrica/dao/ConnessioneDao.java
@@ -19,7 +19,8 @@ public class ConnessioneDao {
         Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/postgres";
         Properties propr = new Properties();
-        /* credenziali */
+            propr.setProperty("user", "postgres");
+        propr.setProperty("password", "admin");
         Connection conn = DriverManager.getConnection(url, propr);
         return conn;
     }
